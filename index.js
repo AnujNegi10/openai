16c3c1297c844e79979f26d9e9bb4dd0e98b6a1a
AnujNegi10 <168225793+AnujNegi10@users.noreply.github.com>
2024-12-12 16:27:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Import required modules
-import express from 'express';
+import express from "express";
 import OpenAI from "openai";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import  cors  from 'cors';
 dotenv.config({
-    path: ".env"
-})
-const api = process.env.OPENAI_API_KEY 
+  path: ".env",
+});
+const api = process.env.OPENAI_API_KEY;
 
 const openai = new OpenAI({
-    apiKey : api
+  apiKey: api,
 });
 
 // Create an instance of the Express application
@@ -18,44 +19,46 @@ const app = express();
 app.use(express.json());
 
 // Set up a port for the server to listen on
-const port = 3000;
-
+const port = 5001;
+app.use(cors())
 // Define a POST route
-app.post('/info',async (req, res) => {
-    // Extract data from the request body
-    
-    const completion = await openai.chat.completions.create({
-        model: "gpt-4o",
-        messages: [
-            {"role": "user", 
-            "content": "summarixze this"+req.body.message +"under 50 words"
-        }
-        ]
-    });
+app.post("/info", async (req, res) => {
+  // Extract data from the request body
 
-    res.json({
-        message : completion.choices[0].message
-    })
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "user",
+        content: "create a timeline for this news " + req.body.message +"in as brief as possible in sentences  "
+      },
+    ],
+  });
+
+  res.json({
+    message: completion.choices[0].message,
+  });
 });
 
-app.post('/translate',async (req, res) => {
-    // Extract data from the request body
-    
-    const completion = await openai.chat.completions.create({
-        model: "gpt-4o",
-        messages: [
-            {"role": "user", 
-            "content": "translate this message "+req.body.message +"to english"
-        }
-        ]
-    });
+app.post("/translate", async (req, res) => {
+  // Extract data from the request body
 
-    res.json({
-        message : completion.choices[0].message
-    })
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "user",
+        content: "translate this message to english " + req.body.message + "if only in english then return the same sentence ",
+      },
+    ],
+  });
+
+  res.json({
+    message: completion.choices[0].message,
+  });
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(Server is running on http://localhost:${port});
 });
